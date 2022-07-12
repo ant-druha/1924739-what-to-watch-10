848cbafc08c35b1c70a3a8cc3cc9bd3bbd27ee9a
Andrey Dernov <andrey.dernov@gmail.com>
2022-07-12 20:39:53 +0200

    Adds routing for 404 page.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -6,26 +6,26 @@ import {MyList} from '../../pages/my-list-screen/my-list';
 import {Film} from '../../pages/film-screen/film';
 import {Review} from '../../pages/submit-review-screen/review';
 import {Player} from '../../pages/player-scren/player';
+import {NotFound} from '../../pages/not-found-screen/not-found';
 
 export const App = (): JSX.Element => (
   <BrowserRouter>
     <Routes>
-      <Route path={AppRoute.Root}
+      <Route
+        path={AppRoute.Root}
         element={<MainScreen filmTitle='The Grand Budapest Hotel' filmGenre='Drama' filmReleaseDate='2014'/>}
       />
-      <Route path={AppRoute.Login}
-        element={<Login/>}
-      />
-      <Route path={AppRoute.MyList}
-        element={<MyList/>}
-      />
+      <Route path={AppRoute.Login} element={<Login/>}/>
+      <Route path={AppRoute.MyList} element={<MyList/>}/>
       <Route path={AppRoute.Films}>
+        <Route index element={<NotFound/>}/>
         <Route path={AppRoute.Film}>
           <Route index element={<Film/>}/>
           <Route path={AppRoute.Review} element={<Review/>}/>
         </Route>
       </Route>
       <Route path={AppRoute.Player} element={<Player/>}/>
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
   </BrowserRouter>
 );
