7c6d06fd08b50854677c8d1717c92cda75bd65c7
Andrey Dernov <andrey.dernov@gmail.com>
2022-07-21 08:28:13 +0200

    Extracts AddReviewForm form into a component and uses state to store form input data.

---
+++ project/src/components/add-review-form/add-review-form.tsx
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+
+export const AddReviewForm = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [rating, setRating] = useState<number>();
+
+  const handleInputChange = (evt: React.MouseEvent) => {
+    const target = evt.target as HTMLInputElement;
+    if (target && target.checked) {
+      setRating(Number(target.value));
+    }
+  };
+
+  return (
+    <div className="add-review">
+      <form action="#" className="add-review__form">
+        <div className="rating">
+          <div className="rating__stars">
+            <input className="rating__input" id="star-10" type="radio" name="rating" value="10" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-10">Rating 10</label>
+
+            <input className="rating__input" id="star-9" type="radio" name="rating" value="9" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-9">Rating 9</label>
+
+            <input className="rating__input" id="star-8" type="radio" name="rating" value="8" onClick={handleInputChange} checked/>
+            <label className="rating__label" htmlFor="star-8">Rating 8</label>
+
+            <input className="rating__input" id="star-7" type="radio" name="rating" value="7" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-7">Rating 7</label>
+
+            <input className="rating__input" id="star-6" type="radio" name="rating" value="6" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-6">Rating 6</label>
+
+            <input className="rating__input" id="star-5" type="radio" name="rating" value="5" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-5">Rating 5</label>
+
+            <input className="rating__input" id="star-4" type="radio" name="rating" value="4" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-4">Rating 4</label>
+
+            <input className="rating__input" id="star-3" type="radio" name="rating" value="3" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-3">Rating 3</label>
+
+            <input className="rating__input" id="star-2" type="radio" name="rating" value="2" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-2">Rating 2</label>
+
+            <input className="rating__input" id="star-1" type="radio" name="rating" value="1" onClick={handleInputChange}/>
+            <label className="rating__label" htmlFor="star-1">Rating 1</label>
+          </div>
+        </div>
+
+        <div className="add-review__text">
+          <textarea className="add-review__textarea" name="review-text" id="review-text"
+            placeholder="AddReviewScreen text"
+          >
+          </textarea>
+          <div className="add-review__submit">
+            <button className="add-review__btn" type="submit">Post</button>
+          </div>
+
+        </div>
+      </form>
+    </div>
+  );
+};
