e0b52a1ce79219d5e971b01c0415325f62ae6ddd
Andrey Dernov <andrey.dernov@gmail.com>
2022-08-06 15:17:59 +0200

    Adds token header for http API requests.

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,9 +1,24 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import {getToken} from './token';
 
 const SERVER_BASE_URL = 'https://10.react.pages.academy/wtw';
 const REQUEST_TIMEOUT = 5000;
 
-export const createAPI = (): AxiosInstance => axios.create({
-  baseURL: SERVER_BASE_URL,
-  timeout: REQUEST_TIMEOUT
-});
+export const createAPI = (): AxiosInstance => {
+  const instance = axios.create({
+    baseURL: SERVER_BASE_URL,
+    timeout: REQUEST_TIMEOUT
+  });
+
+  instance.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
+  });
+
+  return instance;
+};
